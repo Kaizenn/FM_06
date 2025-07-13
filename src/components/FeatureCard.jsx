d38830810aecc2ac775a533b3e489dc1f920a34e
Olivier <olivierdeplanque@gmail.com>
2025-07-14 01:12:56 +0200

    Fix card width desktop breakpoint

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -11,7 +11,7 @@ export default function FeatureCard({ title, description, area, image, borderCol
       whileHover={{ scale: 1.05, transition: { duration: 0.1, ease: 'easeOut' } }}
       whileTap={{ scale: 0.95, transition: { duration: 0.05, ease: 'easeOut' } }}
       variants={itemVariants}
-      className={`${borderColor} border-secondary-cyan mb-8 max-w-[19.625rem] cursor-pointer rounded-lg border-t-4 bg-white p-8 shadow-lg shadow-blue-100 md:mb-0 lg:w-[21.875rem] lg:max-w-full`}
+      className={`${borderColor} border-secondary-cyan mb-8 max-w-[19.625rem] cursor-pointer rounded-lg border-t-4 bg-white p-8 shadow-lg shadow-blue-100 md:mb-0 xl:w-[21.875rem] xl:max-w-full`}
       style={{ gridArea: area }}
     >
       <h2 className="text-preset3 mb-1.5">{title}</h2>
